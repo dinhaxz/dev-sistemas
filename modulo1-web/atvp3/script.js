f0e8211c3d47866b6f55142f9b194393008f3117
Deborah Azevedo <deborahmendonca124@gmail.com>
2026-08-14 14:14:29 -0300

    atividadeP3

---
+++ modulo1-web/atvp3/script.js
@@ -0,0 +1,25 @@
+// Variáveis numéricas
+let precoProduto = 20;
+let quantidade = 3;
+
+// Cálculos
+let total = precoProduto * quantidade;
+let dobro = total * 2;
+let resto = total % 2;
+
+// Exibição no console
+console.log("Total da compra:", total);
+console.log("Dobro do total:", dobro);
+console.log("Resto da divisão por 2:", resto);
+
+// Variáveis booleanas
+let cupomValido = true;
+let freteGratis = false;
+
+// Operadores lógicos
+let algumBeneficio = cupomValido || freteGratis;
+let todosBeneficios = cupomValido && freteGratis;
+
+// Exibição dos benefícios no console
+console.log("Algum benefício (OU):", algumBeneficio);
+console.log("Todos os benefícios (E):", todosBeneficios);
